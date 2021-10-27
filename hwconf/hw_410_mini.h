79a05f8cdc6d308dbfdaa6b112f80376613914a6
ncerzzk <huangzzk@bupt.edu.cn>
2021-10-27 09:54:56 +0800

    add hw410_mini support
    Signed-off-by: ncerzzk <huangzzk@bupt.edu.cn>

---
+++ hwconf/hw_410_mini.h
@@ -0,0 +1,195 @@
+/*
+	Copyright 2015 Benjamin Vedder	benjamin@vedder.se
+
+	This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    */
+
+#ifndef HW_410_MINI_H_
+#define HW_410_MINI_H_
+
+#define HW_NAME					"410"
+#define INVERTED_SHUNT_POLARITY
+#define COMM_USE_USB 0
+
+#define HW_CANH_PORT			GPIOA
+#define HW_CANH_PIN				11
+#define HW_CANL_PORT			GPIOA
+#define HW_CANL_PIN				12
+#define HW_CAN_GPIO_AF			GPIO_AF_CAN1
+#define HW_CAN_DEV				CAND1
+
+// Macros
+#define ENABLE_GATE()			
+#define DISABLE_GATE()			
+#define DCCAL_ON()				
+#define DCCAL_OFF()				
+#define IS_DRV_FAULT()			0
+
+#define LED_GREEN_ON()			palSetPad(GPIOC, 9)
+#define LED_GREEN_OFF()			palClearPad(GPIOC, 9)
+#define LED_RED_ON()			
+#define LED_RED_OFF()			
+
+/*
+ * ADC Vector
+ *
+ * 0:	IN0		SENS3
+ * 1:	IN1		SENS2
+ * 2:	IN2		SENS1
+ * 3:	IN8		CURR2
+ * 4:	IN9		CURR1
+ * 5:	IN3		TEMP_MOSFET
+ * 6:	Vrefint
+ * 7:	IN6		ADC_EXT2
+ * 8:	IN12	AN_IN
+ * 9:	IN4		TX_SDA_NSS
+ * 10:	IN5     ADC_EXT
+ * 11:	IN3 	TEMP_MOTOR
+ */
+
+#define HW_ADC_CHANNELS			12
+#define HW_ADC_INJ_CHANNELS		2
+#define HW_ADC_NBR_CONV			4
+
+// ADC Indexes
+#define ADC_IND_SENS1			0
+#define ADC_IND_SENS2			1
+#define ADC_IND_SENS3			2
+#define ADC_IND_CURR1			8
+#define ADC_IND_CURR2			9
+#define ADC_IND_VIN_SENS		11
+#define ADC_IND_EXT				10
+#define ADC_IND_EXT2			7
+#define ADC_IND_TEMP_MOS		5
+#define ADC_IND_TEMP_MOTOR		4
+#define ADC_IND_VREFINT			6
+
+// ADC macros and settings
+
+// Component parameters (can be overridden)
+#ifndef V_REG
+#define V_REG					3.3
+#endif
+#ifndef VIN_R1
+#define VIN_R1					100000.0
+#endif
+#ifndef VIN_R2
+#define VIN_R2					10000.0
+#endif
+#ifndef CURRENT_AMP_GAIN
+#define CURRENT_AMP_GAIN		50.0
+#endif
+#ifndef CURRENT_SHUNT_RES
+#define CURRENT_SHUNT_RES		0.003
+#endif
+
+// Input voltage
+#define GET_INPUT_VOLTAGE()		((V_REG / 4095.0) * (float)ADC_Value[ADC_IND_VIN_SENS] * ((VIN_R1 + VIN_R2) / VIN_R2))
+
+// Voltage on ADC channel
+#define ADC_VOLTS(ch)			((float)ADC_Value[ch] / 4095.0 * V_REG)
+
+// NTC Termistors
+#define NTC_RES(adc_val)		((4095.0 * 10000.0) / adc_val - 10000.0)
+#define NTC_TEMP(adc_ind)		25//(1.0 / ((logf(NTC_RES(ADC_Value[adc_ind]) / 10000.0) / 3434.0) + (1.0 / 298.15)) - 273.15)
+
+#define NTC_RES_MOTOR(adc_val)	(10000.0 / ((4095.0 / (float)adc_val) - 1.0)) // Motor temp sensor on low side
+#define NTC_TEMP_MOTOR(beta)	25//(1.0 / ((logf(NTC_RES_MOTOR(ADC_Value[ADC_IND_TEMP_MOTOR]) / 10000.0) / beta) + (1.0 / 298.15)) - 273.15)
+
+// Double samples in beginning and end for positive current measurement.
+// Useful when the shunt sense traces have noise that causes offset.
+#ifndef CURR1_DOUBLE_SAMPLE
+#define CURR1_DOUBLE_SAMPLE		0
+#endif
+#ifndef CURR2_DOUBLE_SAMPLE
+#define CURR2_DOUBLE_SAMPLE		0
+#endif
+
+// UART Peripheral
+#define HW_UART_DEV				SD5
+#define HW_UART_GPIO_AF			GPIO_AF_UART5
+#define HW_UART_TX_PORT			GPIOC
+#define HW_UART_TX_PIN			12
+#define HW_UART_RX_PORT			GPIOD
+#define HW_UART_RX_PIN			2
+
+// ICU Peripheral for servo decoding
+#define HW_ICU_TIMER			TIM3
+#define HW_ICU_TIM_CLK_EN()		RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE)
+#define HW_ICU_DEV				ICUD3
+#define HW_ICU_CHANNEL			ICU_CHANNEL_2
+#define HW_ICU_GPIO_AF			GPIO_AF_TIM3
+#define HW_ICU_GPIO				GPIOB
+#define HW_ICU_PIN				5    // 与编码器冲突了
+
+// I2C Peripheral
+#define HW_I2C_DEV				I2CD2
+#define HW_I2C_GPIO_AF			GPIO_AF_I2C2
+#define HW_I2C_SCL_PORT			GPIOB
+#define HW_I2C_SCL_PIN			10
+#define HW_I2C_SDA_PORT			GPIOB
+#define HW_I2C_SDA_PIN			11
+
+// Hall/encoder pins
+#define HW_HALL_ENC_GPIO1		GPIOB
+#define HW_HALL_ENC_PIN1		6    // 与编码器冲突
+#define HW_HALL_ENC_GPIO2		GPIOB
+#define HW_HALL_ENC_PIN2		7
+#define HW_HALL_ENC_GPIO3		GPIOC
+#define HW_HALL_ENC_PIN3		11
+#define HW_ENC_TIM				TIM4
+#define HW_ENC_TIM_AF			GPIO_AF_TIM4
+#define HW_ENC_TIM_CLK_EN()		RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE)
+#define HW_ENC_EXTI_PORTSRC		EXTI_PortSourceGPIOC
+#define HW_ENC_EXTI_PINSRC		EXTI_PinSource11
+#define HW_ENC_EXTI_CH			EXTI15_10_IRQn
+#define HW_ENC_EXTI_LINE		EXTI_Line11
+#define HW_ENC_EXTI_ISR_VEC		EXTI15_10_IRQHandler
+#define HW_ENC_TIM_ISR_CH		TIM4_IRQn
+#define HW_ENC_TIM_ISR_VEC		TIM4_IRQHandler
+
+// SPI pins
+#define HW_SPI_DEV				SPID3
+#define HW_SPI_GPIO_AF			GPIO_AF_SPI3
+#define HW_SPI_PORT_NSS			GPIOB
+#define HW_SPI_PIN_NSS			6
+#define HW_SPI_PORT_SCK			GPIOB
+#define HW_SPI_PIN_SCK			3
+#define HW_SPI_PORT_MOSI		GPIOB
+#define HW_SPI_PIN_MOSI			5
+#define HW_SPI_PORT_MISO		GPIOB
+#define HW_SPI_PIN_MISO			4
+
+// Measurement macros
+#define ADC_V_L1				ADC_Value[ADC_IND_SENS1]
+#define ADC_V_L2				ADC_Value[ADC_IND_SENS2]
+#define ADC_V_L3				ADC_Value[ADC_IND_SENS3]
+#define ADC_V_ZERO				(ADC_Value[ADC_IND_VIN_SENS] / 2)
+
+// Macros
+#define READ_HALL1()			palReadPad(HW_HALL_ENC_GPIO1, HW_HALL_ENC_PIN1)
+#define READ_HALL2()			palReadPad(HW_HALL_ENC_GPIO2, HW_HALL_ENC_PIN2)
+#define READ_HALL3()			palReadPad(HW_HALL_ENC_GPIO3, HW_HALL_ENC_PIN3)
+
+// Setting limits
+#define HW_LIM_CURRENT			-100.0, 100.0
+#define HW_LIM_CURRENT_IN		-100.0, 100.0
+#define HW_LIM_CURRENT_ABS		0.0, 150.0
+#define HW_LIM_VIN				6.0, 57.0
+#define HW_LIM_ERPM				-200e3, 200e3
+#define HW_LIM_DUTY_MIN			0.0, 0.1
+#define HW_LIM_DUTY_MAX			0.0, 0.95
+#define HW_LIM_TEMP_FET			-40.0, 110.0
+
+#endif /* HW_410_H_ */
